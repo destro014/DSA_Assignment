1452444e6d85ebd8b0c14bca34b2ba881fe12c55
destro014 <japmc11@gmail.com>
2016-08-13 17:23:10 +0545

    assignment no. 8

---
+++ assignment_08/binary_sort.c
@@ -0,0 +1,82 @@
+//Binary tree
+#include<stdlib.h>
+#include<stdio.h>
+
+struct bin_tree {
+int data;
+struct bin_tree * right, * left;
+};
+typedef struct bin_tree node;
+
+void insert(node ** tree, int val)
+{
+    node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = (node *)malloc(sizeof(node));
+        temp->left = temp->right = NULL;
+        temp->data = val;
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+void print_inorder(node * tree)
+{
+    if (tree)
+    {
+        print_inorder(tree->left);
+        printf("%d\n",tree->data);
+        print_inorder(tree->right);
+    }
+}
+
+
+void deltree(node * tree)
+{
+    if (tree)
+    {
+        deltree(tree->left);
+        deltree(tree->right);
+        free(tree);
+    }
+}
+
+
+void main()
+{
+    node *root;
+    node *tmp;
+    //int i;
+
+    root = NULL;
+    /* Inserting nodes into tree */
+    insert(&root, 9);
+    insert(&root, 4);
+    insert(&root, 15);
+    insert(&root, 6);
+    insert(&root, 12);
+    insert(&root, 17);
+    insert(&root, 2);
+
+    /* Printing nodes of tree in sorted form*/
+
+
+    printf("Sorted array:\n");
+    print_inorder(root);
+
+
+
+    /* Deleting all nodes of tree */
+    deltree(root);
+}
